7617027c92dfa9dc589f4a60ba99ef6dbbdb9812
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-02-01 12:06:01 -0700

    Started the loop and added comments to the code that exists

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,12 +1,14 @@
-# TO-DO: Complete the selection_sort() function below 
+# TO-DO: Complete the selection_sort() function below
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        cur_index = i # What our second loop will use for its first index
+        smallest_index = cur_index # How we will designate as the smallest
+        #number and restart loops if smaller numbers are found while using the second loop
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        for j in range(cur_index, len(arr)): # Compares
+            if arr[smallest_index]>arr[j]:
+        # (hint, can do in 3 loc)
 
 
 
